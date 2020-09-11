f4b8e28f736f7f685aca05784781c8ddd7815e5f
Matthijs Heuten <matthijsheuten@appnormal.com>
2020-09-11 13:15:18 +0200

    remove last value check

--- lib/src/pin_input.dart
+++ lib/src/pin_input.dart
@@ -38,8 +38,6 @@ class _PinInputState extends State<PinInput> {
   var values = Map<int, String>();
   List<Widget> pinInputs = [];
 
-  var previousValue;
-
   @override
   void initState() {
     super.initState();
@@ -105,9 +103,8 @@ class _PinInputState extends State<PinInput> {
       });
     }
 
-    if (widget.onCodeCompleted != null && splitted.length == widget.pinInputsAmount && value != previousValue) {
+    if (widget.onCodeCompleted != null && splitted.length == widget.pinInputsAmount) {
       widget.onCodeCompleted(widget.pinCodeValue);
-      previousValue = value;
     }
   }
 
